7919f9ee7ef6c111a7597df990b55b9b602952c8
DaviRic <davi.r.cruvel@gmail.com>
2025-03-05 13:43:20 -0400

    Script oficial para a transformação dos dados. Já trata os dados de 2022 corretamente. Ainda falta tratar alguns casos para ficar completo

--- scripts/pipelines/1_transformation_fruit_main.py
+++ scripts/pipelines/1_transformation_fruit_main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import os
 
 fruit_main_path = "data/raw/fruits/fruit-main"
 processed_path = "datasets"
@@ -57,3 +58,54 @@ def processed_fruit_excel(file_path, fruit_name):
 
     return df_final
 
+raw_fruit_path = "data/raw/fruits"
+processed_fruit_path = "datasets/processed_fruits"
+os.makedirs(processed_fruit_path, exist_ok=True)
+
+# Função para processar todas as planilhas de todas as subpastas
+def process_all_fruit_excels():
+    for subfolder in os.listdir(raw_fruit_path):
+        # Está criando um caminho para acessar a subpasta dentro da pasta principal
+        # O intúito dessa linha é acessar as subpastas "fruit-main", "fruit-2020", etc
+        subfolder_path = os.path.join(raw_fruit_path, subfolder)
+
+        # Verifica se é uma pasta
+        if os.path.isdir(subfolder_path):
+            # A variável "save_path" serve para salvar o caminho onde os dados processados serão salvos
+            '''
+            Pego o caminho para a pasta onde os dados processados ficarão salvos e coloco dentro da pasta
+            com o nome que está em armazenado em "subfolder", que no caso é "fruit-main" ou "fruit-2020"
+            ou "fruit-2013", etc
+            '''
+            save_path = os.path.join(processed_fruit_path, subfolder)
+            # Se não existir a pasta, então cria para salvar os dados nela
+            os.makedirs(save_path, exist_ok=True)
+
+            # Vou iterar sobre cada pasta que contem as planilhas excel para transformá-las
+            for file in os.listdir(subfolder_path):
+                # Verifica se o arquivo termina com ".xlsx"
+                if file.endswith(".xlsx"):
+                    # Extrai o nome da fruta pelo nome do arquivo e coloca todas as letras em minúscula
+                    fruit_name = file.split("-")[0].lower()
+                    # Crio uma string com o nome do caminho do arquivo ".xlsx" para passar ele por parâmetro na função
+                    # "processed_fruit_excel"
+                    file_path = os.path.join(subfolder_path, file)
+
+                    print(f"Processando {file} de {subfolder}...")
+
+                    # Essa é a linha que transforma os dados. Remodelando a estrutura das colunas para melhorar o
+                    # entendimento dos dados
+                    df_fruit = processed_fruit_excel(file_path, fruit_name)
+
+                    # Verifica se gerou um dataframe ao rodar "processed_fruit_excel"
+                    if df_fruit is not None:
+                        # Cria a string com o nome do caminho onde serão salvos os dados transformados
+                        output_file = os.path.join(save_path, file)
+                        # Transforma o dataframe em uma rquivo excel e salva no diretório cujo caminho está em "output_file"
+                        df_fruit.to_excel(output_file, index=False)
+                        print(f"Salvo: {output_file}")
+
+    print("Todas as planilhas foram processadas e salvas!")
+
+# Executa o processamento
+process_all_fruit_excels()
